071d0404db7e702028540a5340711ba76d106c5d
Racha Sathwik Sai Abhinav <rachasathwik18@gmail.com>
2024-08-21 08:13:49 +0530

    2.9*: The Phonebook Step 4

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -2,37 +2,60 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('');
-  const[newNumber,setNewNumber] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  
 
   const handleNewContact = (event) => {
     event.preventDefault();
-    if(persons.find(person => person.name === newName)){
+    if(persons.find(person => person.name===newName)){
       alert(`${newName} is already in the phonebook`);
       return;
     }
-    setPersons(persons.concat({name:newName,number:newNumber}));
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: newName.length,
+    };
+    setPersons(persons.concat(newPerson));
     setNewName('');
     setNewNumber('');
   }
+
+  const filtered = persons.filter(p =>
+    p.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter with name</p>
+      <input
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      
+      <h2>add a New</h2>
       <form onSubmit={handleNewContact}>
         <div>
-          <div>name: <input value={newName} onChange={(e)=> setNewName(e.target.value)}/></div>
-          <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/><br/>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person)=><p key={person.name}>{person.name} {person.number}</p>)}
-      </div>
+      {filtered.map(p => (
+          <p key={p.id}>{p.name} {p.number}</p>
+        ))}
+      
     </div>
   )
 }
