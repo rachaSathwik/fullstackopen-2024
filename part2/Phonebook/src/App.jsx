b56369c5a0797c16e244aa1fea410acc08baa367
Racha Sathwik Sai Abhinav <rachasathwik18@gmail.com>
2024-08-23 22:18:19 +0530

    2.11: The Phonebook Step 6 --Fetched data from server(db.json) using useEffect

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-
+import axios from 'axios';
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const handleResolve = (response) => {
+      console.log("Promise Fulfilled");
+        setPersons(response.data);
+    }
+    const handleReject = (error) => {
+        console.log("Promise Rejected");
+        console.log(error);
+    }
+    console.log("effect");
+    axios
+      .get('http://localhost:3001/persons')
+      .then(handleResolve)
+      .catch(handleReject)
+  },[])
 
   const handleNewContact = (event) => {
     event.preventDefault();
@@ -29,7 +40,7 @@ const App = () => {
     setNewName('');
     setNewNumber('');
 }
-
+console.log(persons.length);
 return (
   <div>
     <h2>Phonebook</h2>
