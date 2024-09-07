3ca29d696a6d8ce078789e580c2d2dfd13a712ce
Racha Sathwik Sai Abhinav <rachasathwik18@gmail.com>
2024-09-07 16:24:03 +0530

    3.2: Phonebook backend step 2

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const persons = [
@@ -24,10 +24,17 @@ const persons = [
   },
 ];
 
-
-
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `
+  );
 });
 
 const PORT = 3001;
