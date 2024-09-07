771ec1e4d571c370e2cfd18031d37880095e501c
Racha Sathwik Sai Abhinav <rachasathwik18@gmail.com>
2024-09-07 16:52:05 +0530

    3.4: Phonebook backend step 4

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -45,6 +45,12 @@ app.get('/api/persons/:id',(req,res) => {
     }else{
         res.status(404).end();
     }
+})
+
+app.delete('/api/persons/:id',(req,res) => {
+    const id = req.params.id;
+    persons = persons.filter(person => person.id!==id);
+    res.status(204).end();
 })
 
 const PORT = 3001;
